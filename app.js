bb8e6eb3a008f50ec24454210e4bff2c1c1fa2d5
Rizwan <eminence.rizwan@gmail.com>
2023-03-15 19:33:23 +0530

    jwt auth

--- app.js
+++ app.js
@@ -1,24 +1,21 @@
 
 const express = require('express');
+require('./DB/config')
 const app = express();
-const mongoose = require('mongoose');
-const dotenv = require('dotenv').config();
-const PORT = 6000;
 app.use(express.json())
+const PORT = 6000;
 
 
-mongoose
-    .connect(process.env.MONGO_URL)
-    .then(() => console.log('DBConnection Succesfull'))
-    .catch((err) => console.log("err", err));
 
-    app.get('/api/test',()=>{
-        console.log(`test is succes`);
-    })
 
 // router 
-const userRouter = require('./routes/users')
+const userRouter = require('./routes/users');
+const authRouter =require('./routes/Auth')
+
+// api
+
 app.use('/api/user',userRouter);
+app.use('/api/auth',authRouter);
 
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
---
+++ routes/VeriFyToken.js
@@ -0,0 +1,44 @@
+const jwt = require("jsonwebtoken");
+
+const verifyToken = (req, res, next) => {
+  const authHeader = req.headers.token;
+  if (authHeader) {
+    const token = authHeader.split(" ")[1];
+    console.log(token);
+    jwt.verify(token, process.env.JWT_SEC, (err, user) => {
+      if (err) 
+      {
+        res.status(403).send("Token is not valid!");
+      }else{
+        req.user = user;
+        next();
+      }
+    });
+  } else {
+    return res.status(401).send("You are not authenticated!");
+  }
+};
+const verifyTokenAndAuthorization = (req, res, next) => {
+    verifyToken(req, res, () => {
+      if (req.user && req.user.id == req.params.id ) {
+        next();
+      } else {
+        res.status(403).send("You are not allowed to do that!");
+      }
+    });
+  };
+const verifyTokenAndAdmin = (req, res, next) => {
+  verifyToken(req, res, () => {
+    if (req.user.isAdmin) {
+      next();
+    } else {
+      res.status(403).send("You are not alowed to do that!");
+    }
+  });
+};
+
+module.exports = {
+  verifyToken,
+  verifyTokenAndAuthorization,
+  verifyTokenAndAdmin,
+};
